7e788e1fe261df8edf8b4371b8f217126b5d35de
guyhadad19 <guyhadad@mail.tau.ac.il>
2024-11-10 19:11:04 +0200

    added parsing support for for statements

--- parser/__init__.py
+++ parser/__init__.py
@@ -1,7 +1,7 @@
-from pyrsercomb import token, regex, fix, Parser, string, lift3, const, lift2
+from pyrsercomb import token, regex, fix, Parser, string, lift3, const, lift2, lift5
 
 from models import (
-    Identifier, IntExpr, BinaryIntExpr, BinaryBoolExpr, BoolExpr, Comparison, Assignment, If, While,
+    Identifier, IntExpr, BinaryIntExpr, BinaryBoolExpr, BoolExpr, Comparison, Assignment, If, While, For,
     Statement)
 
 lpar, rpar = token(string("(")), token(string(")"))
@@ -28,7 +28,7 @@ def def_int_expr(
 
 int_expr = fix(def_int_expr)
 
-comparison_op = token(regex(r"(<|<=|>=|>|=|!=)"))
+comparison_op = token(regex(r"(<=|<|>=|>|=|!=)"))
 comparison = (int_expr & comparison_op & int_expr)[lift3(Comparison)]
 
 false = token(string("false"))[const(False)]
@@ -36,7 +36,7 @@ true = token(string("true"))[const(True)]
 
 boolean = true ^ false
 
-bool_op = token(regex(r"(&&|\\|\\|)"))
+bool_op = token(regex(r"(&&|\|\|)"))
 
 
 def def_bool_expr(parser: Parser[str, BoolExpr]) -> Parser[str, BoolExpr]:
@@ -52,7 +52,7 @@ bool_expr = fix(def_bool_expr)
 
 skip = token(string("skip"))[const(None)] << semicolon
 assignment = (ident << assign & int_expr)[lift2(Assignment)] << semicolon
-
+no_semicolon_assignment = (ident << assign & int_expr)[lift2(Assignment)]
 
 def def_statement(parser: Parser[str, Statement]) -> Parser[str, Statement]:
     if_statement = (
@@ -67,9 +67,17 @@ def def_statement(parser: Parser[str, Statement]) -> Parser[str, Statement]:
             & parser
     )[lift3(While)]
 
+    for_statement = (
+            token(string("for")) >> assignment
+            & bool_expr << semicolon
+            & assignment << token(string("do"))
+            & token(lbracket >> bool_expr << rbracket)
+            & parser
+    )[lift5(For)]
+
     block = lbrace >> ~parser << rbrace
 
-    return block ^ if_statement ^ while_statement ^ skip ^ assignment
+    return block ^ if_statement ^ while_statement ^ for_statement ^ skip ^ assignment
 
 
 statement = fix(def_statement)
